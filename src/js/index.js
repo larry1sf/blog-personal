e5be944941ecb2429c8cc42a1202a0c964c8b15f
larry <larryycel@gmail.com>
2024-08-31 19:06:57 -0500

    NUEVA UI

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 let contador = 0
 
-function reformat_hors(e = "" | null) {
+function reformat_hors({ e = "" | null }) {
   const dif_hors = e.split(":")
 
   let hors12 = parseInt(dif_hors[0])
@@ -17,74 +17,63 @@ const btn_add_log = document.getElementById("add-log")
 const table_body = document.getElementById("body-table")
 const btn_close_all = document.getElementById(`btn-close-all`)
 
-const btn_enviar = document
-  .getElementById("form-logs")
-  .addEventListener("submit", (event) => {
-    event.preventDefault()
-    const form_dt = new FormData(event.target)
-    const form = {}
-    const form_res = []
-    // let cuenta_registros = 0
+document.getElementById("form-logs").addEventListener("submit", (event) => {
+  ww
+  event.preventDefault()
+  const form_dt = new FormData(event.target)
+  // const form = {}
+  // const form_res = []
 
-    form_dt.forEach((value, key) => {
-      const x = reformat_hors(value)
-      // form_res.push(form)
-      const h = key.startsWith("h")
-        ? (form[key] = x)
-        : (form[key] = value)
-      // const r = key.endsWith(contador)
-      //   ?
-      // cuenta_registros++
-    })
-    console.log(form)
+  // form_dt.forEach((value, key) => {
+  //   const x = reformat_hors(value)
 
-    // form_res.push(form)
-  })
+  //   key.startsWith("h") ? (form[key] = x) : (form[key] = value)
+  // })
+  console.log(...form_dt)
+  // console.log(form_res)
+
+  // form_res.push(form)
+})
 
 function add_log() {
   contador = table_body.querySelectorAll("tr").length + 1
 
   const linea = document.createElement("tr")
   linea.innerHTML = `
-                                <td class='ps-2 '>
-                                    <label for="actividad-${contador} floatingInput" class="form-label">
-                                        <input required type="text" title="recibir registro" name="actividad-registro-${contador}"
-                                            class="p-1 form-control" id="actividad-${contador} floatingInput" placeholder="Trabajo...">
-                                    </label>
-                                </td>
+    <td class='ps-2 '>
+        <label for="actividad-${contador} floatingInput" class="form-label">
+            <input required type="text" title="recibir registro" name="actividad-registro-${contador}"
+                class="p-1 form-control" id="actividad-${contador} floatingInput" placeholder="Trabajo...">
+        </label>
 
-                                <td>
-                                    <label for="hora-registro-${contador}" class="form-label">
-                                        <input required type="time" title="hora de registro" name="hora-registro-${contador}"
-                                            class="p-1 form-control" id="hora-registro-${contador}">
-                                    </label>
-                                </td>
+    <td>
+        <label for="hora-registro-${contador}" class="form-label">
+            <input required type="time" title="hora de registro" name="hora-registro-${contador}"
+                class="p-1 form-control" id="hora-registro-${contador}">
+        </label>
 
-                                <td>
-                                    <label for="hora-finalizacion-${contador}" class="form-label">
-                                        <input required type="time" title="hora de finalizacion" placeholder="00/00"
-                                            name="hora-finalizacion-${contador}" class="p-1 form-control" id="hora-finalizacion-${contador}">
-                                    </label>
-                                </td>
+    <td>
+        <label for="hora-finalizacion-${contador}" class="form-label">
+            <input required type="time" title="hora de finalizacion" placeholder="00/00"
+                name="hora-finalizacion-${contador}" class="p-1 form-control" id="hora-finalizacion-${contador}">
+        </label>
 
-                                <td class="pe-0 pe-lg-2 ">   
-                                    <div class="form-check form-switch d-inline-block ms-0 ms-lg-3 col-3 w-50" >
-                                        <label for="checkeo-${contador}" title="checkeo" class="form-check-label ">
-                                            <input type="checkbox" role="switch" name="checkeo-${contador}" class="form-check-input"
-                                                id="checkeo-${contador}">
-                                        </label>
-                                    </div>
-                                    <div data-bs-theme="dark" class=' d-inline-block position-relative end-0 me-0 me-lg-2 col-2 '>
-                                        <button id="btn-close-${contador}" title="btn-close" type="button"
-                                            class="btn-close"></button>
-                                    </div>
-                                </td>
-                                
-
+    <td class="pe-0 pe-lg-2 ">   
+        <div class="form-check form-switch d-inline-block ms-0 ms-lg-3 col-3 w-50" >
+            <label for="checkeo-${contador}" title="checkeo" class="form-check-label ">
+                <input type="checkbox" role="switch" name="checkeo-${contador}" class="form-check-input"
+                    id="checkeo-${contador}">
+            </label>
+        </div>
+        <div data-bs-theme="dark" class=' d-inline-block position-relative end-0 me-0 me-lg-2 col-2 '>
+            <button id="btn-close-${contador}" title="btn-close" type="button"
+                class="btn-close"></button>
+        </div>
+    </td>
 `
   table_body.append(linea)
 
-  const btn_close = document
+  document
     .getElementById(`btn-close-${contador}`)
     .addEventListener("click", function () {
       contador--
@@ -135,6 +124,6 @@ function add_log() {
     table_body.append(x)
   })
 }
-btn_add_log.addEventListener("click", add_log)
 
+btn_add_log.addEventListener("click", add_log)
 add_log()
